88e9e3e44ae387be5caae75a67e72aca9ba4228c
Robert Mock <sompmismyname@gmail.com>
2013-08-24 18:20:21 -0700

    Bases can build stuff

--- Assets/Scripts/BaseScript.js
+++ Assets/Scripts/BaseScript.js
@@ -4,25 +4,52 @@ var buildGuidePlane:Transform;
 var selected:boolean;
 var constructables:GameObject[];
 var arrow:Transform;
+var buildClock:float;
+var currentBuild:GameObject;
+var nextInQueue:GameObject;
+var outputLocation:Vector3;
+var timer:Timer;
+var controller:GameObject;
 
 function Start () {
-
+timer=controller.GetComponent(Timer);
 }
 
 function Update () {
-var stickVector=Vector2(Input.GetAxisRaw("Horizontal1"),Input.GetAxisRaw("Vertical1"));
-Debug.Log(stickVector);
+if (timer.turn=="exec"){
+if(currentBuild){
+buildClock+=Time.deltaTime;
+if(buildClock>9.9){
+buildClock-=9.9;
+var newUnit=Instantiate(currentBuild,transform.position+outputLocation,Quaternion.identity);
+newUnit.GetComponent(DoStuff).controller=controller;
+}
+}}
+if(nextInQueue&&!currentBuild){
+currentBuild=nextInQueue;
+nextInQueue=null;
+}
+
+
+
 if(selected){
+var stickVector=Vector2(Input.GetAxisRaw("Horizontal1"),Input.GetAxisRaw("Vertical1"));
 buildGuidePlane.position.y=transform.position.y;
 if (stickVector.magnitude>.9){
 var stickAngle=Vector2.Angle(Vector2.right,stickVector);
 if (Input.GetAxisRaw("Vertical1")<0.0){stickAngle=360.0-stickAngle;}
 Debug.Log(stickAngle);
-var ToBuildIndex=Mathf.RoundToInt(stickAngle/360*constructables.Length);
-stickAngle=toBuildIndex*360/constructable.Length;
-	arrow.rotation = Quaternion.LookRotation(-stickAngle);
-	arrow.localPosition = stickAngle;
-	//build constructables.ToBuildIndex
+var toBuildIndex=Mathf.RoundToInt(stickAngle/360*constructables.Length);
+stickAngle=toBuildIndex*360/constructables.Length;
+stickVector=Vector2(Mathf.Cos(stickAngle*Mathf.PI/180),Mathf.Sin(stickAngle*Mathf.PI/180));
+var stickVector3=Vector3(stickVector.x,0,stickVector.y);
+	arrow.rotation = Quaternion.LookRotation(-stickVector3);
+	arrow.localPosition = stickVector3;
+	Debug.Log(toBuildIndex);
+	if(toBuildIndex==constructables.Length){toBuildIndex=0;}
+	nextInQueue=constructables[toBuildIndex];
+var stick2Vector=Vector3(Input.GetAxisRaw("Horizontal2"),0,Input.GetAxisRaw("Vertical2"));
+if(stick2Vector.magnitude>.5){outputLocation=stick2Vector.normalized;}
 }
 }
 else{buildGuidePlane.position.y=20;}
--- Assets/Scripts/ListOfEntities.js
+++ Assets/Scripts/ListOfEntities.js
@@ -11,6 +11,7 @@ var homeBase:BaseScript;
 
 function Start(){
 timer=gameObject.GetComponent(Timer);
+homeBase.timer=timer;
 }
 
 function Update(){
